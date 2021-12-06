e6c50b00a926785887836fe69e18d631c89a9345
Keegan Donley <kd@keegandonley.com>
2021-12-06 12:41:29 -0600

    clean up and add readme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div>Hello, world!</div>;
+	return (
+		<div className="flex items-center w-screen h-screen justify-center">
+			<p>
+				Hello, world! This is a sample{' '}
+				<span className="font-bold">NextJS app</span> with{' '}
+				<span className="font-bold text-blue-600">TailwindCSS</span> and{' '}
+				<span className="font-bold text-indigo-600">Typescript</span>.
+			</p>
+		</div>
+	);
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  purge: ['./pages/**/*', './components/**/*'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: ['./pages/**/*', './components/**/*'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
